111f31f8f8a333f6683d17c9dc51bbc06cc840c3
h-mid <amlibrino@live.fr>
2020-04-02 17:33:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/recylerview/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.recylerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<ExampleItem> exampleList = new ArrayList<>();
+        exampleList.add(new ExampleItem(R.drawable.admin,"hamid","admin"));
+        exampleList.add(new ExampleItem(R.drawable.guest,"tristan","client"));
+        exampleList.add(new ExampleItem(R.drawable.user,"jason ","commercial"));
+    }
+}
